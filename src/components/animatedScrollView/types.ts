72541bdcb837249d14021f897ee6d3183933435e
ekosh02 <polumontum@gmail.com>
2025-04-29 22:47:03 +0500

    added instructions for usage

--- src/components/animatedScrollView/types.ts
+++ src/components/animatedScrollView/types.ts
@@ -2,7 +2,35 @@ import { ReactNode } from 'react'
 import { ScrollViewProps } from 'react-native'
 
 export interface AnimatedScrollViewProps extends ScrollViewProps {
+  /**
+   * Component displayed as the main header before scrolling.
+   *
+   * @example
+   * topHeaderComponent={<Text>Top Header</Text>}
+   */
   topHeaderComponent?: ReactNode
+
+  /**
+   * Component displayed as the header after scrolling past a threshold.
+   *
+   * @example
+   * scrolledHeaderComponent={<Text>Scrolled Header</Text>}
+   */
   scrolledHeaderComponent?: ReactNode
+
+  /**
+   * Component displayed inside the content view that will be animated with scaling and translation effects.
+   *
+   * @example
+   * contentView={<Text>Animated Content</Text>}
+   */
   contentView?: ReactNode
+
+  /**
+   * Regular static content inside the ScrollView.
+   *
+   * @example
+   * <View><Text>Static Content</Text></View>
+   */
+  children?: ReactNode
 }
