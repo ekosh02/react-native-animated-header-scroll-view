34e371622a981c2d86b07becce297a6f686c365e
ekosh02 <polumontum@gmail.com>
2025-07-19 04:19:08 +0500

    added safe area insets

--- src/components/headerView/types.ts
+++ src/components/headerView/types.ts
@@ -1,9 +1,18 @@
 import { ReactNode } from 'react'
-import { ViewProps } from 'react-native'
+import { ColorValue, ViewProps, ViewStyle } from 'react-native'
 
 export interface HeaderViewProps extends ViewProps {
   topHeaderComponent?: ReactNode
   scrolledHeaderComponent?: ReactNode
   isScrolledToHeaderEnd: boolean
+  headerBackgroundColor?: ColorValue
+  useSafeArea?: boolean
   onHeaderLayout: ViewProps['onLayout']
+}
+
+export type DynamicStylesProps = {
+  top: number
+  opacityTopHeader: ViewStyle['opacity']
+  opacityScrolledHeader: ViewStyle['opacity']
+  headerBackgroundColor: HeaderViewProps['headerBackgroundColor']
 }
